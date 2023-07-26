8c8e6ed45664ebc389ebf2a5634ecfa1144be220
shm-dsgn <77527904+shm-dsgn@users.noreply.github.com>
2023-07-26 22:42:48 +0530

    bug fix

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json());
 //CORS supports secure cross-origin requests and data transfers between browsers and servers
 const corsOptions = {
-  origin: true,
+  origin: 'https://shm-blog-app.onrender.com',
   credentials: true,
 }
 app.use(cors(corsOptions));
